48b2150d786d417655748e8eb3916a65beb9698d
mwpm1161-sketch <cmetaha104@gmail.com>
2026-08-21 15:26:48 +0300

    Export Smart AI Router components

--- averon_import/ai/__init__.py
+++ averon_import/ai/__init__.py
@@ -1,6 +1,8 @@
 from averon_import.ai.config import AiSettings, ProviderSettings
 from averon_import.ai.provider import AiProviderError, OpenAICompatibleProvider
 from averon_import.ai.service import AiCorrectionService
+from averon_import.ai.router import AIRouter, AIRouteDecision
+from averon_import.ai.validator import AICorrectionValidator
 
 __all__ = [
     "AiCorrectionService",
@@ -8,4 +10,7 @@ __all__ = [
     "AiSettings",
     "OpenAICompatibleProvider",
     "ProviderSettings",
+    "AIRouter",
+    "AIRouteDecision",
+    "AICorrectionValidator",
 ]
